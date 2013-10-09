28a1b740b6decc7e4407e13501d74a899e031f01
Renato Gama <renatoargh@gmail.com>
2013-10-09 20:13:26 -0300

    added preParseHook/postParseHook and addRerunIndicator - updated example.js

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ var
 	spawn = require("child_process").spawn,
 	fs = require("fs");
 
-module.exports = function(texString, callback){
+module.exports.parse = function(texString, callback){
 	var outputDirectory = path.join(__dirname, "temp-" + generateGuid());
 	var texFilePath = path.join(outputDirectory, "output.tex");
 	
@@ -13,8 +13,26 @@ module.exports = function(texString, callback){
 		
 		fs.writeFile(texFilePath, texString, function(err){
 			if(err) return callback(err);
-
-			spawnLatexProcess(0, outputDirectory, callback);
+			
+			if(preParseHook){
+				preParseHook({
+					outputDirectory: outputDirectory,
+					texFilePath: texFilePath,
+					texString: texString
+				}, function(err){
+					if(err){
+						rimraf(outputDirectory, function(err){
+							if(err) throw err;
+							
+							return callback(postParseHookError);
+						});
+					}
+					
+					spawnLatexProcess(0, outputDirectory, callback);
+				});
+			}
+			else
+				spawnLatexProcess(0, outputDirectory, callback);
 		});
 	});
 };
@@ -38,24 +56,74 @@ function spawnLatexProcess(attempt, outputDirectory, callback){
 			return callback(new Error("Latex Error! Check your latex string"));
 		}
 		
-		if(outputLog.indexOf("Rerun to get outlines right") !== -1 && attempt < 10)
+		if(shouldRerun(outputLog) && attempt < 10)
 			spawnLatexProcess(++attempt, outputDirectory, callback);
 		else{
 			fs.exists(outputFilePath, function(exists){
 				if(exists){
-					var readStream = fs.createReadStream(outputFilePath);
-					callback(null, readStream);
-					
-					readStream.on("close", function(){
-						rimraf(outputDirectory, function(err){
-							if(err) throw err;
+					if(postParseHook){
+						postParseHook({
+							outputDirectory: outputDirectory,
+							outputFilePath: outputFilePath,
+							outputLog: outputLog
+						}, function(postParseHookError){
+							if(postParseHookError){
+								rimraf(outputDirectory, function(err){
+									if(err) throw err;
+									
+									return callback(postParseHookError);
+								});
+							}
+							else
+								sendPdfStream();
 						});
-					});
+					}
+					else sendPdfStream();
+					
+					function sendPdfStream(){
+						var readStream = fs.createReadStream(outputFilePath);
+						callback(null, readStream);
+						
+						readStream.on("close", function(){
+							rimraf(outputDirectory, function(err){
+								if(err) throw err;
+							});
+						});
+					}
 				}
 				else callback(new Error("Output file was not found - Attempts: " + attempt));
 			});
 		} 
 	});
+}
+
+var postParseHook = null;
+module.exports.setPostParseHook = function(fn){
+	postParseHook = fn;
+};
+
+var preParseHook = null;
+module.exports.setPreParseHook = function(fn){
+	preParseHook = fn;
+};
+
+var rerunIndicators = ["Rerun to get cross-references right", "Rerun to get outlines right"];
+module.exports.getRerunIndicators = function(){
+	return rerunIndicators;
+};
+
+module.exports.addRerunIndicator = function(text){
+	rerunIndicators.push(text);
+};
+
+
+function shouldRerun(outputLog){
+	for(var i = 0; i < rerunIndicators.length; i++){
+		if(outputLog.indexOf(rerunIndicators[i]) !== -1)
+			return true;
+	}
+	
+	return false;
 }
 
 function generateGuid(){
--- example.js
+++ example.js
@@ -3,6 +3,58 @@ var
 	latex = require("./app.js"),
 	fs = require("fs");
 
+//addRerunIndicator
+//-----------------
+//In case the version of latex you use outputs differently you are still able to identify that `pdflatex` must rerun
+latex.addRerunIndicator("If the output log contains this huge string then it means that `pdflatex` must run again");
+
+//setPreParseHook
+//---------------
+//example usage: 
+//	- fetch resources to the temp folder:
+//		* image files
+//		* bib files
+latex.setPreParseHook(function(params, cb){
+	console.log();
+	console.log([
+	             "PRE PARSE HOOK",
+	             "--------------",
+	             "At this time a temporary folder has been created",
+	             "A .tex file has been created within the temp folder",
+	             "And you get some info on the first param",
+	             JSON.stringify(params, null, 2),
+	             "Output .pdf file is not created yet",
+	             "In order to proceed you must call the callback (second parameter)",
+	             "If you pass an error as first parameter the temp folder and its contents will be removed"
+	             ].join("\n"));
+
+	cb(null);
+	//cb(new Error("You can force error for testing purposes!"));
+});
+
+//setPostParseHook
+//----------------
+//example usage: 
+//	- upload resulting .pdf to amazon s3
+//	- send resulting .pdf via email
+latex.setPostParseHook(function(params, cb){
+	console.log();
+	console.log([
+	             "POST PARSE HOOK",
+	             "---------------",
+	             "At this time the temporary folder still exists and all its contents are still there",
+	             "A .pdf file has been created ",
+	             "And you get some info on the first param",
+	             JSON.stringify(params, null, 2),
+	             "In order to proceed you must call the callback (second parameter)",
+	             "If you pass an error as first parameter the temp folder and its contents will be removed"
+	             ].join("\n"));
+	
+	cb();
+	//cb(new Error("You can force error for testing purposes!"));
+});
+
+//You can load from a file or retrieve from database
 var string = [
               "\\documentclass{article}",
               "\\begin{document}",
@@ -10,10 +62,9 @@ var string = [
               "\\end{document}"
               ].join("\n");
 
-latex(string, function(err, readStream){
+latex.parse(string, function(err, readStream){
 	if(err) throw err;
 	
 	var writeStream = fs.createWriteStream("output.pdf");
 	util.pump(readStream, writeStream);
 });
-
